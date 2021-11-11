91205acee4968bd496f80469ad3a719e7b183c90
Richard Halford <richard@xhalford.com>
2021-11-11 11:50:48 +0000

    ⌨ added some mappings for the best yanking, deleting and pasting experience in vim.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,8 +6,14 @@ local keymap = api.nvim_set_keymap
 g.mapleader = [[ ]]
 g.user_emmet_leader_key = [[<C-,>]]
 
--- Yank Line End
-keymap('n', 'Y', 'y$', { noremap = true })
+-- Über Yoinking
+keymap('n', 'Y', 'yg$', { noremap = true })
+keymap('n', '<leader>y', [["+y]], { noremap = true })
+keymap('v', '<leader>y', [["+y]], { noremap = true })
+keymap('n', '<leader>Y', [[gg"+yG]], { noremap = true })
+keymap('x', '<leader>p', [["_dP]], { noremap = true })
+keymap('n', '<leader>d', [["_d]], { noremap = true })
+keymap('v', '<leader>d', [["_d]], { noremap = true })
 
 -- Center Next
 keymap('n', 'n', 'nzzzv', { noremap = true })
@@ -36,17 +42,20 @@ keymap('v', 'J', [[:m '>+1<CR>gv=gv]], { noremap = true })
 keymap('v', 'K', [[:m '<-2<CR>gv=gv]], { noremap = true })
 keymap('i', '<C-j>', '<esc>:m .+1<CR>==', { noremap = true })
 keymap('i', '<C-k>', '<esc>:m .-2<CR>==', { noremap = true })
--- api.nvim_set_keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true })
--- api.nvim_set_keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true })
+-- keymap('n', '<leader>j', ':m .+1<CR>==', { noremap = true })
+-- keymap('n', '<leader>k', ':m .-2<CR>==', { noremap = true })
 
 -- Rezising Windows
 keymap('n', 'zh', ':vertical resize -5<CR>', { noremap = true, silent = true })
 keymap('n', 'zj', ':resize +2<CR>', { noremap = true, silent = true })
 keymap('n', 'zk', ':resize -2<CR>', { noremap = true, silent = true })
 keymap('n', 'zl', ':vertical resize +5<CR>', { noremap = true, silent = true })
---
+
 -- Undotree
 keymap('n', '<leader>z', ':UndotreeToggle<CR>', { noremap = true })
+
+-- Chmod Execute
+keymap('n', '<leader>x', ':!chmod +x %<CR>', { noremap = true, silent = true })
 
 -- List document's URLs
 keymap('n', '<leader>u', ':w<Home>silent <End> !urlview<CR>', { noremap = true })
