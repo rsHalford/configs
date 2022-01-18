d42a3ed04356dfd69566e7015e188b0152bfd5e8
Richard Halford <richard@xhalford.com>
2022-01-18 19:55:44 +0000

    feat(nvim): diffget keybindings when using fugitive

--- .config/nvim/lua/config/fugitive.lua
+++ .config/nvim/lua/config/fugitive.lua
@@ -2,5 +2,7 @@ local keymap = vim.api.nvim_set_keymap
 local g = vim.g
 
 keymap('n', '<leader>gg', ':G<CR>', { noremap = true })
+keymap('n', '<leader>gh', ':diffget //2<CR>', { noremap = true })
+keymap('n', '<leader>gl', ':diffget //3<CR>', { noremap = true })
 
 g.fugitive_summary_format = '%d %s'
