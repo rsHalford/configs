230b9fa8d7f9384bc36b8b9d1044a2a3a7eb69d3
Richard Halford <richard@xhalford.com>
2022-01-20 19:13:55 +0000

    feature(nvim): identify and assign python path for pyright setup

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -49,13 +49,42 @@ null_ls.setup {
   on_attach = on_attach,
 }
 
+-- python
+local util = require 'lspconfig.util'
+local path = util.path
+
+local function get_python_path(workspace)
+  -- Use activated venv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use venv from poetry in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system 'poetry env info --path')
+    return path.join(venv, 'bin', 'python')
+  end
+
+  -- Fallback to system Python.
+  return vim.fn.exepath 'python3' or vim.fn.exepath 'python' or 'python'
+end
+
+lspconfig.pyright.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  before_init = function(_, config)
+    config.settings.python.pythonPath = get_python_path(config.root_dir)
+  end,
+}
+
+-- lsp servers
 local servers = {
   'bashls',
   'cssls',
   'gopls',
   'html',
   'jsonls',
-  'pyright',
   'svelte',
   'tsserver',
   'vimls',
